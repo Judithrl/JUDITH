ee61b34b8e7e81020e9b9c616370429503272755
Diego J. Moguer Castillo <diego_dj_m@hotmail.com>
2019-10-16 11:40:06 +0200

    correcccion campo que se actualiza

--- UPDATE.sql
+++ UPDATE.sql
@@ -8,7 +8,7 @@ WHERE nombre_curso like "Administración de sistemas informáticos en red";
 
 update estadio
 set codigo=4455
-where like "Benito Villamarin";
+where nombre like "Benito Villamarin";
 
 UPDATE estadio
 SET localizacion="Sevilla"
