33a5082a9dde2d8e050dc912435129b1f944140e
Diego J. Moguer Castillo <diego_dj_m@hotmail.com>
2019-10-16 11:08:39 +0200

    he añadido un update a estadio

--- UPDATE.sql
+++ UPDATE.sql
@@ -5,3 +5,7 @@ WHERE Apellido= García;
 UPDATE curso
 SET grado=4
 WHERE nombre_curso like "Administración de sistemas informáticos en red";
+
+update estadio
+set codigo=4455
+where nombre= "Benito Villamarin"
