3aa3708e81171958f568eb91cbbcc92a6bc5229b
Rebeca Suárez <rsbascon1998@gmail.com>
2019-10-16 11:09:18 +0200

    Añado update de estadio

--- UPDATE.sql
+++ UPDATE.sql
@@ -1,7 +1,11 @@
-UPDATE USUARIO
-SET Nombre=Rocío
-WHERE Apellido= García;
+UPDATE usuario
+SET nombre="Rocío"
+WHERE apellido like "García";
 
 UPDATE curso
 SET grado=4
 WHERE nombre_curso like "Administración de sistemas informáticos en red";
+
+UPDATE estadio
+SET localizacion="Sevilla"
+WHERE nombre like "Sánchez Pizjuán";
